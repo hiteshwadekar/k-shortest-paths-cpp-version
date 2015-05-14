3fcbed45d8a909ce1d07fc98a84efa52c9e2d220
hitesh.wadekar@gmail.com <hitesh.wadekar@gmail.com>
2015-05-14 01:54:18 -0400

    Updated main to call yan's k path

--- src/MainP.cpp
+++ src/MainP.cpp
@@ -19,34 +19,65 @@
 using namespace std;
 
 
-void testDijkstraGraph()
+void testDijkstraGraph(int src, int dst)
 {
-	Graph* my_graph_pt = new Graph("data/danYen");
+	Graph* my_graph_pt = new Graph("/home/hitesh-dev/k-shortest-paths-cpp-version/data/test_50");
 	DijkstraShortestPathAlg shortest_path_alg(my_graph_pt);
 	BasePath* result =
 		shortest_path_alg.get_shortest_path(
-			my_graph_pt->get_vertex(46), my_graph_pt->get_vertex(13));
+			my_graph_pt->get_vertex(src), my_graph_pt->get_vertex(dst));
 	result->PrintOut(cout);
 }
 
-void testYenAlg()
+void testYenAlg(int src, int dst, int kpath)
 {
-	Graph my_graph("../data/danYen");
+	Graph my_graph("/home/hitesh-dev/k-shortest-paths-cpp-version/data/test_50");
 
-	YenTopKShortestPathsAlg yenAlg(my_graph, my_graph.get_vertex(46),
-		my_graph.get_vertex(13));
+	YenTopKShortestPathsAlg yenAlg(my_graph, my_graph.get_vertex(src),
+		my_graph.get_vertex(dst));
 
 	int i=0;
-	while(yenAlg.has_next())
+	if(kpath>0)
 	{
-		++i;
-		yenAlg.next()->PrintOut(cout);
+		vector<BasePath*> result_list;
+		yenAlg.get_shortest_paths(my_graph.get_vertex(src),my_graph.get_vertex(dst),kpath,result_list);
+		for(vector<BasePath*>::const_iterator pos=result_list.begin();
+				pos!=result_list.end(); ++pos)
+		{
+			cout <<"Path no " << i << endl;
+			(*pos)->PrintOut(cout);
+			i++;
+		}
+	}
+	else
+	{
+		while(yenAlg.has_next())
+		{
+			++i;
+			yenAlg.next()->PrintOut(cout);
+		}
 	}
 }
 
-int main(...)
+int main(int argc, char *argv[] )
 {
-	cout << "Welcome to the real world!" << endl;
+	if (argc >= 3)
+	{
+		cout << "Welcome to the real world!" << endl;
+		int source = atoi(argv[1]);
+		int destination = atoi(argv[2]);
+		int kpath =0;
+		if(argc>=4)
+		{
+			kpath=atoi(argv[3]);
+		}
+		//testDijkstraGraph();
+		testYenAlg(source, destination,kpath);
+		//testDijkstraGraph(source,destination);
+	}
+	else
+	{
+		cout << "\n Please provide source, destination node and number pathr " << endl;
+	}
 
-	testDijkstraGraph();
 }
